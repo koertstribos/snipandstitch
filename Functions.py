2bc643b69da3fded29ce5c9ee2747b46a0694e16
koertstribos <149697538+koertstribos@users.noreply.github.com>
2025-04-07 16:14:32 +0200

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -5,9 +5,6 @@ def SetLinearCorrection(trials):
     y = np.array([trial.residualCorrection for trial in trials])
     x = np.array([trial.eventCount for trial in trials])
 
-    print(f"residual errors: {x}")
-    print(f"event counts: {y}")
-
     # Calculate the slope using the least-squares formula
     numerator = np.sum(x * y)
     denominator = np.sum(x ** 2)
