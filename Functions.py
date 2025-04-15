30dbf5a3e3c188e73c3babcf7183e46980ba801f
koertstribos <149697538+koertstribos@users.noreply.github.com>
2025-04-15 10:44:17 +0200

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -1,5 +1,16 @@
 import numpy as np
 
+#SetLinearCorrection 
+#this function corrects for linear accumulation of leftover error
+#apply this function on a set of trials 
+#    collected from one participant by one tracker
+#    where the start and end position of gaze in each trial is roughtly the same 
+
+#in: 
+#trials: list of Trial objects
+#
+#out:
+#None (edits trials in-place)
 def SetLinearCorrection(trials):
     y = np.array([trial.residualCorrection for trial in trials])
     x = np.array([trial.eventCount for trial in trials])
