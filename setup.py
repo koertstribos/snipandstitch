7d1056ec69cf9ee09b5faad5e26687077d025599
koertstribos <149697538+koertstribos@users.noreply.github.com>
2025-09-25 18:32:01 +0200

    Add setup.py for package configuration

---
+++ setup.py
@@ -0,0 +1,6 @@
+import setuptoolssetuptools.setup(     
+     name="snipandstitch",     
+     version="1.0.0",
+     python_requires=">=3.6",   
+     packages=['Trial', 'Event', 'Viewer', 'Functions'],
+)
