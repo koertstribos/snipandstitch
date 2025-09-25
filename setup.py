7b18695d452e1751a0d8e8ca865819cd6bcaf5df
koertstribos <149697538+koertstribos@users.noreply.github.com>
2025-09-25 18:39:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ setuptools.setup(
      name="snipandstitch",     
      version="1.0.0",
      python_requires=">=3.6",   
-     packages=['Trial', 'Event', 'Viewer', 'Functions'],
+     packages=['snipandstitch'],
 )
